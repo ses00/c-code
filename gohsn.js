3230560b7cfd926e55144699bfc9d26d2970ad02
jackson <sessntlimboo@gmail.com>
2025-01-13 22:16:27 +0530

    added new file

---
+++ gohsn.js
@@ -0,0 +1,18 @@
+function nasa(){
+    var mul1,mul2;
+
+    mul1= parseInt(document.getElementById("baka").value);
+    mul2= parseInt(document.getElementById("baka2").value);
+
+    mul=mul1*mul2;
+
+    document.getElementById("bnx").value=mul;
+
+    
+
+    
+    
+
+    
+}
+console.log("hello");
